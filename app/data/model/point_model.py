25f5245c4d6a9dbacba4f7807176ea12fe2f6f72
Davi <davifssoares2002@gmail.com>
2024-07-13 20:25:31 -0300

    feat: creating models

---
+++ app/data/model/point_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Double
+from sqlalchemy.orm import relationship
+
+from data.infrastructure.database import Base
+
+class PointModel(Base):
+    __tablename__ = "point"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    address = Column(String(255))
+    lat = Column(Double)
+    lng = Column(Double)
+    alt = Column(Double)
+    description = Column(String(255))
+    point_type_id = Column(Integer)
+    creation_user = Column(Integer)
+    change_date = Column(DateTime)
+    change_user = Column(Integer)
---
+++ app/data/model/user_point_model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Double
+from sqlalchemy.orm import relationship
+
+from data.infrastructure.database import Base
+
+class UserPointModel(Base):
+    __tablename__ = "user_point"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    point_id = Column(Integer)
+    favorite = Column(Boolean)
+    creation_user = Column(Integer)
+    change_date = Column(DateTime)
+    change_user = Column(Integer)
