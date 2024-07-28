587bb944cdcbf9909eac66fce95c25fa61202694
Davi <davifssoares2002@gmail.com>
2024-07-28 16:26:48 -0300

    user dto fix

--- app/presentation/dto/User.py
+++ app/presentation/dto/User.py
@@ -3,10 +3,10 @@ from presentation.dto.CreatePoint import CreatePoint
 
 class User(BaseModel):
     id: int
-    uuid: str
-    name: str
-    email: str
-    cpf: str
-    cnh: str
-    rg: str
+    uuid: str = ""
+    name: str = ""
+    email: str = ""
+    cpf: str = ""
+    cnh: str = ""
+    rg: str = ""
     user_type_id: int
