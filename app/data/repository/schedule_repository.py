165bbda842e898ea33fbeb1a8216ab4c7e12db10
Davi <davifssoares2002@gmail.com>
2024-08-20 19:21:06 -0300

    fix

--- app/data/repository/schedule_repository.py
+++ app/data/repository/schedule_repository.py
@@ -26,69 +26,69 @@ class ScheduleRepository():
             self.db.rollback()
             raise ValueError("Erro ao fazer a leitura no sistema")
         
-    def create_schedule_destiny_school(self, schedule: CreateSchedule, driver: UserModel, vehicle: VehicleModel, points: List[PointModel], school: PointModel):
-        try:
-            creation_date = datetime.now()
+    # def create_schedule_destiny_school(self, schedule: CreateSchedule, driver: UserModel, vehicle: VehicleModel, points: List[PointModel], school: PointModel):
+    #     try:
+    #         creation_date = datetime.now()
 
-            schedule = ScheduleModel(name=schedule.schedule_name, initial_date=creation_date,
-                                    end_date = schedule.end_date, real_initial_date = creation_date,
-                                    description=f"Viagem de ida para {school.name} - {creation_date.date()}",
-                                    schedule_type_id=1, creation_user=driver.id)
+    #         schedule = ScheduleModel(name=schedule.schedule_name, initial_date=creation_date,
+    #                                 end_date = schedule.end_date, real_initial_date = creation_date,
+    #                                 description=f"Viagem de ida para {school.name} - {creation_date.date()}",
+    #                                 schedule_type_id=1, creation_user=driver.id)
             
-            self.db.add(schedule)
+    #         self.db.add(schedule)
 
-            schedule_vehicle = ScheduleVehicleModel(schedule_id=schedule.id, vehicle_id=vehicle.id, creation_user= driver.id)
+    #         schedule_vehicle = ScheduleVehicleModel(schedule_id=schedule.id, vehicle_id=vehicle.id, creation_user= driver.id)
 
-            self.db.add(schedule_vehicle)
+    #         self.db.add(schedule_vehicle)
 
-            schedule_user = ScheduleUserModel(schedule_id=schedule.id, user_id=driver.id, creation_user=driver.id)
+    #         schedule_user = ScheduleUserModel(schedule_id=schedule.id, user_id=driver.id, creation_user=driver.id)
 
-            self.db.add(schedule_user)
+    #         self.db.add(schedule_user)
 
-            for point in points:
-                schedule_point = SchedulePointModel(schedule_id=schedule.id, point_id=point.id, creation_user=driver.id)
-                self.db.add(schedule_point)
+    #         for point in points:
+    #             schedule_point = SchedulePointModel(schedule_id=schedule.id, point_id=point.id, creation_user=driver.id)
+    #             self.db.add(schedule_point)
 
-            schedule_point_school = SchedulePointModel(schedule_id=schedule.id, point_id=school.id, description=f"Destino: Escola {school.name}" ,creation_user=driver.id)
+    #         schedule_point_school = SchedulePointModel(schedule_id=schedule.id, point_id=school.id, description=f"Destino: Escola {school.name}" ,creation_user=driver.id)
 
-            self.db.add(schedule_point_school)
+    #         self.db.add(schedule_point_school)
 
-            self.db.commit()
-            return schedule.id
-        except:
-            self.db.rollback()
-            raise ValueError("Erro ao fazer o registro no sistema")
+    #         self.db.commit()
+    #         return schedule.id
+    #     except:
+    #         self.db.rollback()
+    #         raise ValueError("Erro ao fazer o registro no sistema")
         
-    def create_schedule_origin_school(self, schedule: CreateSchedule, driver: UserModel, vehicle: VehicleModel, points: List[PointModel], school: PointModel):
-        try:
-            creation_date = datetime.now()
+    # def create_schedule_origin_school(self, schedule: CreateSchedule, driver: UserModel, vehicle: VehicleModel, points: List[PointModel], school: PointModel):
+    #     try:
+    #         creation_date = datetime.now()
 
-            schedule = ScheduleModel(name=schedule.schedule_name, initial_date=creation_date,
-                                    end_date = schedule.end_date, real_initial_date = creation_date,
-                                    description=f"Viagem de volta de {school.name} - {creation_date.date()}",
-                                    schedule_type_id=1, creation_user=driver.id)
+    #         schedule = ScheduleModel(name=schedule.schedule_name, initial_date=creation_date,
+    #                                 end_date = schedule.end_date, real_initial_date = creation_date,
+    #                                 description=f"Viagem de volta de {school.name} - {creation_date.date()}",
+    #                                 schedule_type_id=1, creation_user=driver.id)
             
-            self.db.add(schedule)
+    #         self.db.add(schedule)
 
-            schedule_vehicle = ScheduleVehicleModel(schedule_id=schedule.id, vehicle_id=vehicle.id, creation_user= driver.id)
+    #         schedule_vehicle = ScheduleVehicleModel(schedule_id=schedule.id, vehicle_id=vehicle.id, creation_user= driver.id)
 
-            self.db.add(schedule_vehicle)
+    #         self.db.add(schedule_vehicle)
 
-            schedule_user = ScheduleUserModel(schedule_id=schedule.id, user_id=driver.id, creation_user=driver.id)
+    #         schedule_user = ScheduleUserModel(schedule_id=schedule.id, user_id=driver.id, creation_user=driver.id)
 
-            self.db.add(schedule_user)
+    #         self.db.add(schedule_user)
 
-            schedule_point_school = SchedulePointModel(schedule_id=schedule.id, point_id=school.id, description=f"Origem: Escola {school.name}" ,creation_user=driver.id)
+    #         schedule_point_school = SchedulePointModel(schedule_id=schedule.id, point_id=school.id, description=f"Origem: Escola {school.name}" ,creation_user=driver.id)
 
-            self.db.add(schedule_point_school)
+    #         self.db.add(schedule_point_school)
 
-            for point in points:
-                schedule_point = SchedulePointModel(schedule_id=schedule.id, point_id=point.id,creation_user=driver.id, initial_date=creation_date)
-                self.db.add(schedule_point)
+    #         for point in points:
+    #             schedule_point = SchedulePointModel(schedule_id=schedule.id, point_id=point.id,creation_user=driver.id, initial_date=creation_date)
+    #             self.db.add(schedule_point)
             
-            self.db.commit()
-            return schedule.id
-        except:
-            self.db.rollback()
-            raise ValueError("Erro ao fazer o registro no sistema")
+    #         self.db.commit()
+    #         return schedule.id
+    #     except:
+    #         self.db.rollback()
+    #         raise ValueError("Erro ao fazer o registro no sistema")
 
