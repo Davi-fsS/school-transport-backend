29552e8c6b71e9c0ba3d281bfcdc90547b53b5fd
Davi <davifssoares2002@gmail.com>
2024-12-02 22:26:51 -0300

    firebase token validation

---
+++ app/presentation/middleware/firebase_auth_middleware.py
@@ -0,0 +1,34 @@
+from fastapi import HTTPException, Request
+from starlette.middleware.base import BaseHTTPMiddleware
+from firebase_admin import credentials, auth
+import firebase_admin
+import os
+
+class FirebaseAuthMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app):
+        super().__init__(app)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        project_root = os.path.join(current_dir, "../../../")
+        json_path = os.path.join(project_root, "auth-firebase.json")
+        
+        cred = credentials.Certificate(json_path)
+        if not firebase_admin._apps:
+            firebase_admin.initialize_app(cred)
+
+    async def dispatch(self, request: Request, call_next):
+        if request.url.path.startswith("/docs") or request.url.path.startswith("/openapi.json"):
+            return await call_next(request)
+        
+        token = request.headers.get("Authorization")
+        if not token or not token.startswith("Bearer "):
+            raise HTTPException(status_code=401, detail="Token não fornecido ou inválido.")
+        
+        id_token = token.split("Bearer ")[1]
+        try:
+            decoded_token = auth.verify_id_token(id_token)
+            request.state.user = decoded_token
+        except Exception as e:
+            raise HTTPException(status_code=401, detail="Token inválido ou expirado.")
+        
+        response = await call_next(request)
+        return response
